ed29f893a5219f54a62d03f5a29a54417130f478
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-07-30 23:55:13 +0500

    Advanced Python

---
+++ Classes/1-Class/class-1.py
@@ -0,0 +1,60 @@
+# input
+
+a = input("Enter a number: ")   # By default, input returns a string
+print("This is string:", a)
+
+# type conversion
+b = int(a)   # Convert string to integer
+c = float(a)    # Convert string to float
+# bool
+
+
+print(type(a))
+print("this is string", a, b, c)
+print("this is number", b)
+print("this is float number", c)
+
+
+
+a = 10 
+b = 20.5
+a + b  # expression
+
+print("This is integer:", a)
+
+
+
+
+for i in range(1, 11):  
+    print("This is number:", i)  # this is indentation
+
+
+
+
+# Data Types
+# int, float, str, bool, list, tuple, set, dict
+
+a = 10
+f = 20.5
+s = "Hello"
+l = [1, 2, 3, 4, 5]
+t = (1, 2, 7)  
+complex = 3 + 4j
+
+
+
+
+# Operators
+# Arithmetic Operators
+# +, -, *, /, //, %, **
+a = 3
+b = 7        
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)  
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
