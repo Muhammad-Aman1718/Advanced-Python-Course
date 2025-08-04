c11db9b239fb2246cba9e1996339bd1be9d6a17e
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-04 19:38:00 +0500

    Class 3

---
+++ Classes/python/Class_3/string.py
@@ -0,0 +1,43 @@
+# b = "fdjslkajflkd"
+
+
+# a = """
+# jfdklsajflkjdsalf
+# gjsgfd
+# ngfjsf
+# gjslkj
+
+# """
+
+# name = "Roger"
+# name += " is a good dog"
+# print(name)  # Roger is a good dog
+# str()
+
+
+# a = 90
+# print(type(a))
+# b = str(a)
+# print(type(b))
+
+
+# print("Roger is " + str(8) + " years old")  # Roger is 8 years old
+
+
+# name = input("enter your name : ")
+# user2 = input("enter your name : ")
+ 
+# print(name.lower(), user2)
+
+
+
+# print( "this is string methods" , dir(str))
+# print( "this is number methods" , dir(int))
+# print( "this is boolean methods" , dir(bool))
+
+
+
+# a = "am\"an"
+# # e = "nd"
+
+# print(a)
