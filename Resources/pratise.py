392f04be3bb735b83ba91bbadb81078761abcc9b
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-07 20:46:02 +0500

    pracitse

--- Resources/pratise.py
+++ Resources/pratise.py
@@ -21,12 +21,38 @@ list = [3, 2, 1, 4, 5]
 # list.sort(key=len )
 
 # new_list = sorted(list)
-new_list = list.sort()
+# new_list = list.sort()
 
 
-print(list)
-print(new_list)
+# print(list)
+# print(new_list)
 
 
 # statically typed
 # Dynamically typed
+
+
+# a = "hello"
+# print(id(a))       # Example: 140344944
+
+# a += " world"
+# print(a)           # 'hello world'
+# print(id(a))       # New id → new object was created
+
+
+# a = 10
+# print(id(a))  # e.g., 9789120
+
+# a += 1
+# print(id(a))  # different id → new object created
+
+
+# my_list = [1, 2, 3]
+# print(id(my_list))  # e.g., 139932928
+
+# my_list.append(4)
+# print(id(my_list))  # same id → object modified
+
+
+my_tuple = (1, 4, 5, 2, 3)
+print(my_tuple[2:4])
