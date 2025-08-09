be05caed4038d4164a639e7b57af23d7e423298f
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-09 20:10:35 +0500

    tuples

--- Classes/python/Class_6/tuples.py
+++ Classes/python/Class_6/tuples.py
@@ -1,7 +1,7 @@
-a = [1, 2, 3, 4, 5]
-b = [1, 2, 3, 4, 5]
-tuple = (4, 3, 4)
-# tuple2 = (2, 3, 4)
+# a = [1, 2, 3, 4, 5]
+# b = [1, 2, 3, 4, 5]
+tuples = (4, 3, 4)
+tuple2 = (2, 3, 4)
 
 # number = 10
 # number += 10
@@ -9,9 +9,9 @@ tuple = (4, 3, 4)
 # str = "aman"
 # print(id(str))
 # str += " zain"
-
 # print(id(str))
+
 # print(number)
 
 
-print(dir(tuple))
+# print(tuples.count(4))
--- Resources/script.js
+++
@@ -1,7 +0,0 @@
-a = [1, 2, 3, 4, 5];
-b = [1, 2, 3, 4, 5];
-tuple = (2, 3, 4);
-tuple2 = (2, 3, 4);
-
-console.log(a == b);
-console.log(tuple == tuple2);
