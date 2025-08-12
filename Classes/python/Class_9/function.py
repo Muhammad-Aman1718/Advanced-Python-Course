c75fb4b226f6d077aecb810cdce6cc68aa824326
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-12 20:54:15 +0500

    function

--- Classes/python/Class_9/function.py
+++ Classes/python/Class_9/function.py
@@ -1,10 +1,31 @@
-
-
 # def add():
 
-a = 3
-b = 5
+# def prnt():
+#     print("Hello World")
 
-add =  a + b
-print(add)
-    
+
+# def add(num1: int, num2: int):   # Formal parameter
+#     add = a + b
+#     # print(add)
+#     return add
+
+
+# a = 3
+# b = 5
+
+# # add = a +
+# p = add(a, b)   # Actual parameter
+# print(p)
+
+
+def arrfunc(arr: list[int]):
+    # for i in arr:
+    #     print(i, end=" ")
+    arr.sort()
+    return arr
+
+
+arr: list[int] = [1, 5, 2, 3, 4, 6]
+
+
+print(arrfunc(arr))
