3f9a4f4dabf150caaae5a38386074a8c39b8014e
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-19 20:27:37 +0500

    pass break continue

---
+++ Classes/python/Class_10/pass_break_continue.py
@@ -0,0 +1,31 @@
+# # break / continue
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)  # prints 0,1,2
+
+
+# # pass
+# for x in range(3):
+#     if x == 2:
+#         pass  # placeholder
+#     print("this ", x)
+
+
+
+
+
+
+
+
+
+# for i in range(1, 11):
+#     if i == 6:
+#         continue
+#     print(i, end=" ")
+
+
+
+score = int(input("Enter your score: "))
+
+print(" you are pass") if score>=50 else  print(" you are fail") 
