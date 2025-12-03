2d237f15a02725c708df0ea478134aafd30e1d8a
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-03 21:49:10 +0500

    Exception Handling

---
+++ Classes/python/Class_12/exceptionHandling.py
@@ -0,0 +1,20 @@
+# try:
+#     # risky code
+# except:
+#     # error handle
+# else:
+# finally:
+
+
+# n = 10
+# res = n / 0
+
+# print(res)
+
+
+try:
+    n = 0
+    res = 100 / "12"
+
+except Exception as e:
+    print(e)
