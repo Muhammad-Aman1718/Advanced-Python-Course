9a221e1e1ba8dd77a5713476014f20541705787f
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-11 20:56:16 +0500

    file handling

---
+++ Classes/python/Class_13/file.py
@@ -0,0 +1,21 @@
+# file = open("sample.txt", "a+")
+
+# # data = file.write("this is manager")
+# # read = file.read()
+
+# readl1 = file.readline()
+# readl2 = file.readline()
+# readl3 = file.readline()
+
+
+# print(readl1)
+# print(readl2)
+# print(readl3)
+# # print(read)
+
+
+# file.close()
+
+
+# with open("sample.txt", "a+") as f:
+    
